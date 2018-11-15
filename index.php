75ca5192d9fe7ea2c197d3b60ff20a4963427efa
Unknown <deimos.krijgsman@gmail.com>
2018-11-15 23:33:55 +0100

    start

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+ <link rel="apple-touch-icon" href="icon.png">
+    <meta name="apple-mobile-web-app-title" content="DARE">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <style>
+    
+        body {
+            background-color: red;
+        }
+    
+    </style>
+</head>
+<body>
+    
+</body>
+
+</html>
