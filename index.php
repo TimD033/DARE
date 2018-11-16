53ccdf6a5c7d348690f2f64dbac8f2507876ba78
Unknown <deimos.krijgsman@gmail.com>
2018-11-16 15:07:40 +0100

    fvcx

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="viewport" content="user-scalable=no" />
+   
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
     
 
 
@@ -71,7 +85,7 @@
 
     <div class="video">
         <video id="myVideo" autoplay loop muted playsinline>
-            <source src="https://r4---sn-5hnekn7l.googlevideo.com/videoplayback?ipbits=0&signature=4FCDEF4A6F9F68DE6B6499175C5C05FA815CCB58.0E55995F32799A7718571B763263C5965A660CC9&dur=877.960&mime=video%2Fmp4&itag=136&source=youtube&mm=31%2C29&mn=sn-5hnekn7l%2Csn-5hne6ns6&requiressl=yes&ip=145.120.196.136&keepalive=yes&gir=yes&fvip=4&mt=1542371296&mv=m&pl=17&id=o-AMMTKVlY47ToqpPnv6Ed1lO2x77DyMvKhTopgHcMteA4&ei=LbjuW_WDF9WH8gOP87SgBw&ms=au%2Crdu&txp=5532332&c=WEB&key=yt6&initcwndbps=1798750&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C271%2C278%2C313&lmt=1536891389363057&clen=247837353&expire=1542392973&sparams=aitags%2Cclen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&ratebypass=yes" type="video/mp4">
+            <source src="vid/vidd.mp4" type="video/mp4">
         </video>
     </div>
     <div class="bg"></div>
