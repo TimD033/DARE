b5329de34c4f1982797a9a930cd7e7fd49567022
Unknown <deimos.krijgsman@gmail.com>
2018-11-20 18:40:08 +0100

    YEAH LETS GO

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html manifest="manifest.manifest">
 
 <head>
-    <script src="jquery-3.3.1.min.js"></script>
+
     <link rel="apple-touch-icon" href="icon.png">
     <meta name="apple-mobile-web-app-title" content="DARE">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="viewport" content="user-scalable=no" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
    
     <link rel="apple-touch-startup-image" href="https://via.placeholder.com/640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
 
@@ -26,14 +27,22 @@
 
     <style>
 
+        
+        
+        body {
+            background-color: black;
+        }
+        
             #myVideo {
-                position: fixed;
-                top: 0;
-                bottom: -5px;
-                min-width: 100%;
-                min-height: 100%;
-                margin: auto;
-            }
+    position: fixed;
+    top: 0;
+    bottom: -5px;
+    min-width: 100%;
+    min-height: 100%;
+    margin: auto;
+    opacity: .9;
+    filter: saturate(1.6);
+}
         
             .video {
                 position: absolute;
@@ -75,9 +84,111 @@
                 background-image: -webkit-linear-gradient(top, rgb(0, 0, 0) 7%, rgba(0, 0, 0, 0) 95%);
             }
         
-  
+
+        
+#sign {
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    bottom: 364px;
+    width: 550px;
+    -webkit-animation: sign 2s infinite;
+}
+        
+        
+        @-webkit-keyframes sign {
+  from {
+    box-shadow: #ffac92 1px 1px 73px;
+  }
+  50% {
+    box-shadow: #ffac92 1px 1px 120px;
+  }
+            
+            to {
+    box-shadow: #ffac92 1px 1px 73px;
+  }
+}
+        
+#login {
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    bottom: 603px;
+    width: 550px;
+    -webkit-animation: login 2s infinite;
+}
+        
+        
+                @-webkit-keyframes login {
+  from {
+        box-shadow: #51ccf3 1px 1px 73px;
+  }
+  50% {
+    box-shadow: #51ccf3 1px 1px 120px;
+  }
+            
+            to {
+    box-shadow: #51ccf3 1px 1px 73px;
+  }
+}
+        
+    #logo {
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    top: 40px;
+    width: 100%;
+}
+    
+
+        *, *:after, *:before {
+  -moz-box-sizing:border-box; 
+   box-sizing:border-box;
+}
+
+body, html {
+  position: relative; 
+  height: 100%; 
+  width: 100%; 
+  overflow: hidden;
+}
+
+body { 
+  background-color: #fff; 
+  margin: 0; 
+}
+
+        #footer {
+    position: absolute;
+    color: #fff6b3;
+    bottom: 29px;
+    text-align: center;
+    margin: auto;
+    left: 0;
+    right: 0;
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+            width: 80%;
+}
+
+        
+
+
+
+
+
+
+
+
     </style>
-</head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="https://unpkg.com/popmotion/dist/popmotion.global.min.js"></script>
+    </head>
 
 <body>
 
@@ -91,14 +202,22 @@
     <div class="bg"></div>
     <div class="gradient"></div>
 
-    <div class="content">
+        
+<img id="sign" src="signup.png">
+    <img onclick="login()" id="login" src="login.png">
+    <img id="logo" src="logo1.png">
+    
+    
+
+<p id="footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+     
 
 
 
-    </div>
-
-
-
+    <script>
+    
+    
+    </script>
 </body>
 
 </html>
