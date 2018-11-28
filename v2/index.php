d855f9fdea91cf3f09eecd62654c8b4d61eb3424
Unknown <deimos.krijgsman@gmail.com>
2018-11-28 10:28:55 +0100

    v2

---
+++ v2/index.php
@@ -0,0 +1,403 @@
+<html manifest="manifest.manifest">
+
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="apple-touch-icon" href="icon.png">
+    <meta name="apple-mobile-web-app-title" content="DARE">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="viewport" content="user-scalable=no" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500" rel="stylesheet">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
+    <link rel="apple-touch-startup-image" href="https://via.placeholder.com/2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
+
+
+    <style>
+        .bg {
+
+
+
+            position: fixed;
+
+
+            width: 100%;
+            height: 100%;
+            left: 0;
+            position: fixed;
+
+            right: 0;
+            margin: auto;
+            top: 0;
+            bottom: 0;
+            background: linear-gradient(-45deg, #84fab0, #8fd3f4, #84fab0, #8fd3f4);
+            background-size: 400% 400%;
+            -webkit-animation: Gradient 2s ease infinite;
+            -moz-animation: Gradient 2s ease infinite;
+            animation: Gradient 2s ease infinite;
+        }
+
+
+
+        @-webkit-keyframes Gradient {
+            0% {
+                background-position: 0% 50%
+            }
+
+            50% {
+                background-position: 100% 50%
+            }
+
+            100% {
+                background-position: 0% 50%
+            }
+        }
+
+        @-moz-keyframes Gradient {
+            0% {
+                background-position: 0% 50%
+            }
+
+            50% {
+                background-position: 100% 50%
+            }
+
+            100% {
+                background-position: 0% 50%
+            }
+        }
+
+        @keyframes Gradient {
+            0% {
+                background-position: 0% 50%
+            }
+
+            50% {
+                background-position: 100% 50%
+            }
+
+            100% {
+                background-position: 0% 50%
+            }
+        }
+
+
+        .content {
+            width: 95%;
+            height: 97%;
+            left: 0;
+            position: fixed;
+            background-image: linear-gradient(#ace0f9, #b9f1d9);
+            right: 0;
+            margin: auto;
+            top: 0;
+            bottom: 0;
+        }
+
+
+        #logo {
+            left: 0;
+            right: 0;
+            margin: auto;
+            position: fixed;
+            width: 75%;
+            top: 100px;
+            opacity: .9;
+        }
+
+
+
+/* login style */
+
+        .login>#design1 {
+            width: 670px;
+            height: 200px;
+            background-color: rgba(255, 255, 255, 0.25);
+            transform: skew(-31deg);
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 720px;
+            position: fixed;
+            border-radius: 20px;
+            -webkit-animation: Skeww 5s ease infinite;
+            -moz-animation: Skeww 5s ease infinite;
+            animation: Skeww 5s ease infinite;
+        }
+        
+        
+        
+        
+        @-webkit-keyframes Skeww {
+            0% {
+                transform: skew(-31deg);
+            }
+
+            50% {
+                transform: skew(-15deg);
+            }
+
+            100% {
+                transform: skew(-31deg);
+            }
+        }
+
+        @-moz-keyframes Skeww {
+            0% {
+                transform: skew(-31deg);
+            }
+
+            50% {
+                transform: skew(-15deg);
+            }
+
+            100% {
+                transform: skew(-31deg);
+            }
+        }
+
+        @keyframes Skeww {
+            0% {
+                transform: skew(-31deg);
+            }
+
+            50% {
+                transform: skew(-15deg);
+            }
+
+            100% {
+                transform: skew(-31deg);
+            }
+        }
+        
+        
+        
+        
+
+        .login>#design1>#design2 {
+            width: 93%;
+            height: 80%;
+            background-color: rgba(255, 255, 255, 0.2);
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 0;
+            position: fixed;
+            bottom: 0;
+            border-radius: 20px;
+        }
+
+        .login {
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 720px;
+            position: fixed;
+        }
+
+
+        .login>#text {
+            text-align: center;
+            position: fixed;
+            left: 0;
+            right: 0;
+            margin-top: 31px;
+            font-family: 'Roboto', sans-serif;
+            font-size: 113px;
+            font-weight: 100;
+            color: #a5d9c1;
+        }
+
+
+        .login>#design3 {
+            width: 730px;
+            height: 190px;
+            background-color: rgba(255, 255, 255, 0.25);
+            transform: rotate(-4deg);
+            border-radius: 20px;
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 720px;
+            position: fixed;
+            -webkit-animation: rotat 7s ease infinite;
+            -moz-animation: rotat 7s ease infinite;
+            animation: rotat 7s ease infinite;
+        }
+        
+        
+        
+        @-webkit-keyframes rotat {
+            0% {
+                transform: rotate(-4deg);
+            }
+
+            50% {
+                transform: rotate(4deg);
+            }
+
+            100% {
+                transform: rotate(-4deg);
+            }
+        }
+
+        @-moz-keyframes rotat {
+            0% {
+                transform: rotate(-4deg);
+            }
+
+            50% {
+                transform: rotate(4deg);
+            }
+
+            100% {
+                transform: rotate(-4deg);
+            }
+        }
+
+        @keyframes rotat {
+            0% {
+                transform: rotate(-4deg);
+            }
+
+            50% {
+                transform: rotate(4deg);
+            }
+
+            100% {
+                transform: rotate(-4deg);
+            }
+        }
+
+
+
+
+
+
+/* signup style */
+
+        .signup>#design1 {
+            width: 719px;
+            height: 230px;
+            background-color: rgba(255, 255, 255, 0.25);
+            transform: skew(23deg);
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 1070px;
+            position: fixed;
+            border-radius: 20px;
+        }
+
+        .signup>#design1>#design2 {
+            width: 93%;
+            height: 80%;
+            background-color: rgba(255, 255, 255, 0.2);
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 0;
+            position: fixed;
+            bottom: 0;
+            border-radius: 20px;
+        }
+
+        .signup {
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 1100px;
+            position: fixed;
+        }
+
+
+        .signup>#text {
+            text-align: center;
+            position: fixed;
+            left: 0;
+            right: 0;
+            margin-top: 31px;
+            font-family: 'Roboto', sans-serif;
+            font-size: 113px;
+            font-weight: 100;
+            color: #a5d9c1;
+        }
+
+
+        .signup>#design3 {
+            width: 730px;
+            height: 225px;
+            background-color: rgba(255, 255, 255, 0.25);
+            transform: rotate(5deg);
+            border-radius: 20px;
+            left: 0;
+            right: 0;
+            margin: auto;
+            top: 1100px;
+            position: fixed;
+        }
+    </style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="https://unpkg.com/popmotion/dist/popmotion.global.min.js"></script>
+    <script>
+
+
+    </script>
+</head>
+
+<body scroll="no" style="overflow: hidden">
+
+
+    <div class="bg">
+
+    </div>
+
+    <div class="content">
+        <img id="logo" img src="res/logo.png">
+
+
+        <div class="login">
+
+            <div id="design1">
+                <div id="design2">
+
+                </div>
+            </div>
+            <div id="design3"></div>
+            <p id="text">LOGIN</p>
+        </div>
+
+
+
+        <div class="signup">
+
+            <div id="design1">
+                <div id="design2">
+
+                </div>
+            </div>
+            <div id="design3"></div>
+            <p id="text">SIGN UP</p>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
